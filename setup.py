a1dd5f13656dc3a338977bb23ba012ab4ad40ecf
rctrinity <103879453+rctrinity@users.noreply.github.com>
2023-03-19 22:57:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ setuptools.setup(
     description='A terminal dashboard with live updates, displaying bitcoin economic metrics. Inspired by Clark Moody Bitcoin Dashboard.',
     url='https://github.com/rctrinity/biteco',
     packages=['biteco'],
+    install_requires=['rich ==13.3.2',
+                        'python-bitcoinlib == 0.12.0',
+                    ]
     entry_points={
         'console_scripts': ['biteco = biteco.biteco:main'],
     },
