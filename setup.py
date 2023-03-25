7a6de5ab9611105a1e1fa3ee8af27cf1fdd39a53
rctrinity <103879453+rctrinity@users.noreply.github.com>
2023-03-24 23:24:32 -0500

    Fixed the setup to install biteco properly

--- setup.py
+++ setup.py
@@ -1,17 +1,27 @@
-import setuptools
-import biteco
+from setuptools import setup, find_packages
+import os
+from biteco import __version__
 
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.md')) as f:
+    README = f.read()
 
-setuptools.setup(
+setup(
     name='biteco',
-    version=biteco.__version__,
+    version=__version__,
     description='A terminal dashboard with live updates, displaying bitcoin economic metrics. Inspired by Clark Moody Bitcoin Dashboard.',
+    long_description=README,
+        long_description_conten_type='text/markdown',
+        classifers=[
+            "Programming Language :: Python",
+            "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
+        ],
     url='https://github.com/rctrinity/biteco',
-    packages=['biteco'],
+    packages=find_packages(),
     install_requires=['rich ==13.3.2',
                         'python-bitcoinlib == 0.12.0',
                     ],
     entry_points={
-        'console_scripts': ['biteco = biteco.biteco:main'],
+        'console_scripts': ['biteco = biteco.biteco:main', 'gentables = biteco.gentables', 'util = biteco.util'],
     },
 )
