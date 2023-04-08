48231c0539cba22ee53956fa677abbeb1a627f06
Farley <103879453+rctrinity@users.noreply.github.com>
2023-04-07 21:11:09 -0500

    Update biteco.py

--- biteco/biteco.py
+++ biteco/biteco.py
@@ -1,6 +1,6 @@
 '''
  biteco.py
- v0.2.2 Stable
+ v0.2.3 Stable
  Capture Market, Network, Gold, Metrics, and upcoming Events (difficulty and halving)  
  Copyright (c) 2023 Farley
  ___             _       _  _ 
