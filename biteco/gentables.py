c015e950250e9a17878a086416c16e0660470030
Farley <103879453+rctrinity@users.noreply.github.com>
2023-04-09 22:18:14 -0500

    Update gentables.py

--- biteco/gentables.py
+++ biteco/gentables.py
@@ -23,7 +23,15 @@ pnlBrdrStyle = 'dim bold grey62'
 tblTtlStyle = 'white'
 colDescStyle = 'bright_black'
 colValStyle = 'bright_white'
+colValPosChg = 'dim bold green'
+colValNegChg = 'dim bold red'
 tblwidth = 45
+
+nTargetTimespan = 14 * 24 * 60 * 60                     
+nTargetSpacing = 10 * 60                                
+nSecsHour = 60 * 60                                     
+nBlocksHour = (nSecsHour / nTargetSpacing)              
+nInterval = nTargetTimespan / nTargetSpacing
 
 PreviousSelf = None
 
@@ -51,13 +59,13 @@ def addToQueue():
         except:
             pass
 
+
 # Determine value color based on if changes from previous. 
 def ValueColor(current, previous):
-    global colValStyle 
     if current > previous:
-        return 'dim bold green'
+        return colValPosChg
     elif current < previous:
-        return 'dim bold red'
+        return colValNegChg
     else:
         return colValStyle 
 
@@ -108,11 +116,6 @@ class generateDataForTables(object):
                  halvingDate=None, 
                  subsidyEpoch=0, 
                  BestBlockHash=0, 
-                 nTargetTimespan=0, 
-                 nTargetSpacing=0, 
-                 nSecsHour=0, 
-                 nBlocksHour=0, 
-                 nInterval=0, 
                  EpochHead = 0, 
                  bestBlockTimeUnix=0, 
                  bestBlockHeader = None,
@@ -120,7 +123,9 @@ class generateDataForTables(object):
                  mempool_txs = 0,
                  mempool_minfee = 0,
                  mempool_bytes = 0,
+                 pct_rbf = 0,
                  proxy_error = False): 
+        
         global PreviousSelf
         self.proxy_error = proxy_error
         self.proxy=proxy
@@ -128,12 +133,6 @@ class generateDataForTables(object):
             self.proxy=rpc.Proxy()
         
         try:    
-            self.nTargetTimespan = 14 * 24 * 60 * 60                     
-            self.nTargetSpacing = 10 * 60                                
-            self.nSecsHour = 60 * 60                                     
-            self.nBlocksHour = (self.nSecsHour / self.nTargetSpacing)              
-            self.nInterval = self.nTargetTimespan / self.nTargetSpacing
-            
             getAssetPrices = GetAssetPrices()
             
             # Proxy calls
@@ -143,25 +142,31 @@ class generateDataForTables(object):
             chainTxStats = self.proxy.call('getchaintxstats')  
             getBlockChainInfo = self.proxy.call('getblockchaininfo')
             getNetworkInfo = self.proxy.call('getnetworkinfo') 
+            self.getNtwrkHashps = self.proxy.call('getnetworkhashps',-1)  / EXAHASH                    
+            self.get7DNtwrkHashps = self.proxy.call('getnetworkhashps', int(nInterval) >> 1) / EXAHASH    
+            self.get4WNtwrkHashps = self.proxy.call('getnetworkhashps', int(nInterval) << 1) / EXAHASH    
+            self.get1DNtwrkHashps = self.proxy.call('getnetworkhashps', int(nInterval) // 14) / EXAHASH 
+            mempoolinfo = self.proxy.call('getmempoolinfo')
+            rawmempool = self.proxy.getrawmempool(verbose=True)
+            
+            rbf = 0
+            for i in rawmempool:
+                if rawmempool[i]['bip125-replaceable']:
+                    rbf +=1
+            self.pct_rbf = (rbf/len(rawmempool))*100
+            
             self.Connections = getNetworkInfo['connections'] 
             self.ConnectionsIn = getNetworkInfo['connections_in']
-            self.getNtwrkHashps = self.proxy.call('getnetworkhashps',-1)  / EXAHASH                    
-            self.get7DNtwrkHashps = self.proxy.call('getnetworkhashps', int(self.nInterval) >> 1) / EXAHASH    
-            self.get4WNtwrkHashps = self.proxy.call('getnetworkhashps', int(self.nInterval) << 1) / EXAHASH    
-            self.get1DNtwrkHashps = self.proxy.call('getnetworkhashps', int(self.nInterval) // 14) / EXAHASH 
-            mempoolinfo = self.proxy.call('getmempoolinfo')
             self.total_fee = float(mempoolinfo['total_fee'])
             self.mempool_minfee = mempoolinfo['mempoolminfee'] * COIN
             self.mempool_bytes = mempoolinfo['bytes']
             self.mempool_txs = mempoolinfo['size']
-        
             self.bestNonce = self.bestBlockHeader.nNonce
             self.difficulty = self.bestBlockHeader.difficulty/TRILLION
             self.targetBits = self.bestBlockHeader.nBits
             self.bestBlockTimeUnix = time.mktime(datetime.datetime.utcfromtimestamp(self.bestBlockHeader.nTime).timetuple())
             self.coinsMined = float(currentInfo['total_amount'])
             self.UNSPENDABLE = float(currentInfo['total_unspendable_amount'])
-        
             self.BTCPrice = getAssetPrices.Bitcoin_P
             if self.BTCPrice == None:
                 self.BTCPrice = max(PreviousSelf.BTCPrice, 0)     # Work on this and Gold to show last good known price if waiting on server
@@ -176,7 +181,6 @@ class generateDataForTables(object):
                 self.BTCvsGOLDMarketCap = ((self.coinsMined + self.UNSPENDABLE) * self.BTCPrice) / (self.GLDPrice * GOLD_OZ_ABOVE_GROUND) * 100
             except:
                 self.BTCvsGOLDMarketCap = 0
-        
             self.totalTXs = chainTxStats['txcount']
             self.txRatePerSec = chainTxStats['txrate']
             self.txCount = chainTxStats['window_tx_count']         
@@ -184,7 +188,7 @@ class generateDataForTables(object):
             self.MAX_HEIGHT = getBlockChainInfo['blocks']
             self.chainwork = math.log2(int(getBlockChainInfo['chainwork'], 16))
             self.verification = getBlockChainInfo['verificationprogress'] * 100        
-            self.diffEpoch = 1+(self.MAX_HEIGHT // self.nInterval)
+            self.diffEpoch = 1+(self.MAX_HEIGHT // nInterval)
             self.BlockSubsidy = blockSubsidy(self.MAX_HEIGHT)
             self.blockSubsidyValue = self.BlockSubsidy * self.BTCPrice
             self.subsidyEpoch = 1 +(self.MAX_HEIGHT // HALVING_BLOCKS)                        
@@ -211,16 +215,18 @@ class generateDataForTables(object):
         self = ((self.coinsMined + self.UNSPENDABLE) * self.BTCPrice) / BILLION
         return self
 
+    
     def blocksToHalving(self) -> int:
         return int(HALVING_BLOCKS - (self.MAX_HEIGHT % HALVING_BLOCKS))
     
+    
     def AvgBlockTimePrevEpoch(self) -> str:
-        start_2016_block = int((self.MAX_HEIGHT - (self.MAX_HEIGHT % self.nInterval)) - self.nInterval)
-        end_2016_block = int((self.MAX_HEIGHT - (self.MAX_HEIGHT % self.nInterval))-1)
+        start_2016_block = int((self.MAX_HEIGHT - (self.MAX_HEIGHT % nInterval)) - nInterval)
+        end_2016_block = int((self.MAX_HEIGHT - (self.MAX_HEIGHT % nInterval))-1)
         try:
             startBlockTime = self.proxy.getblockheader(self.proxy.getblockhash(start_2016_block))
             endBlockTime = self.proxy.getblockheader(self.proxy.getblockhash(end_2016_block)) 
-            self = str(datetime.timedelta(seconds=(round( (endBlockTime.nTime - startBlockTime.nTime) / self.nInterval,0)))).lstrip("0:")
+            self = str(datetime.timedelta(seconds=(round( (endBlockTime.nTime - startBlockTime.nTime) / nInterval,0)))).lstrip("0:")
             return self
         except:
             self.proxy_error = True
@@ -228,11 +234,11 @@ class generateDataForTables(object):
     
 
     def AvgBlockTimeEpoch(self) -> str:
-        epochStartBlock = int(self.MAX_HEIGHT - (self.MAX_HEIGHT % self.nInterval))
+        epochStartBlock = int(self.MAX_HEIGHT - (self.MAX_HEIGHT % nInterval))
     
         try:
             epochHead = self.proxy.getblockheader(self.proxy.getblockhash(epochStartBlock)) 
-            return str(datetime.timedelta(seconds=(round( (self.bestBlockHeader.nTime - epochHead.nTime) / int(self.MAX_HEIGHT % self.nInterval),0)))).lstrip("0:"), epochHead
+            return str(datetime.timedelta(seconds=(round( (self.bestBlockHeader.nTime - epochHead.nTime) / int(self.MAX_HEIGHT % nInterval),0)))).lstrip("0:"), epochHead
         except:
             self.proxy_error = True
             return '0:00', 0
@@ -242,8 +248,9 @@ class generateDataForTables(object):
         self = str(datetime.timedelta(seconds=(round(time.mktime(datetime.datetime.utcnow().timetuple()) - self.bestBlockTimeUnix, 0)))).lstrip("0:")
         return self
     
+    
     def Retarget(self) -> tuple[float, int, str, int]:
-        nEpochTargetTimespan = int(( self.MAX_HEIGHT % self.nInterval ) * self.nTargetSpacing)
+        nEpochTargetTimespan = int(( self.MAX_HEIGHT % nInterval ) * nTargetSpacing)
         nEpochActualTimespan = int(self.bestBlockHeader.nTime - self.EpochHead.nTime)
 
         if (nEpochActualTimespan < nEpochTargetTimespan/4):
@@ -257,15 +264,15 @@ class generateDataForTables(object):
         bnnew = compact_from_uint256(bnnew)
  
         diffchange = (1-(nEpochActualTimespan / nEpochTargetTimespan)) * 100
-        blocksremain = int(self.nInterval - (self.MAX_HEIGHT % self.nInterval))
-        secsToAdd = (blocksremain  / self.nBlocksHour) * self.nSecsHour
+        blocksremain = int(nInterval - (self.MAX_HEIGHT % nInterval))
+        secsToAdd = (blocksremain  / nBlocksHour) * nSecsHour
         retargetdate = datetime.datetime.fromtimestamp(self.bestBlockTimeUnix + secsToAdd).strftime('%B %d, %Y')
     
         return diffchange, blocksremain, retargetdate, bnnew
     
     
     def HalvingDate(self) -> str:
-        secsToAdd = (self.nBlocksToHalving / self.nBlocksHour) * self.nSecsHour
+        secsToAdd = (self.nBlocksToHalving / nBlocksHour) * nSecsHour
         self = datetime.datetime.fromtimestamp(self.bestBlockTimeUnix + secsToAdd).strftime('%B %d, %Y')
         return self
 
@@ -286,6 +293,7 @@ class dashboard(object):
                 tblMetricEvents=None,
                 tblMempoolInfo=None):
         self.layout=layout
+    
     
     def generateLayout(self) -> Panel:        
         self.layout = Layout()
@@ -314,7 +322,7 @@ class dashboard(object):
         self.layout['right']["bestblock"].size = 9
         self.layout['right']["network"].size = 14
         self.layout['left']["metricevents"].size = 17
-        self.layout['right']["mempoolinfo"].size = 8
+        self.layout['right']["mempoolinfo"].size = 10
         
         r = self.updateLayout()        
         return r
@@ -338,14 +346,14 @@ class dashboard(object):
     
     def generateTable(self) -> tuple[Table, Table, Table, Table, Table, Table, Table, Table]:
         global q, PreviousSelf
-        
+    
         if not q.empty():
             self = q.get()   # Grab from our queue, first in, first out method
         else:
             while not q.full():
                 addToQueue()   
             self = q.get()
-    
+
         if PreviousSelf == None:
             PreviousSelf = self      
 
@@ -353,12 +361,12 @@ class dashboard(object):
         self.tblMarket = Table(title_justify='left', title=' Market', show_header=False, width = tblwidth, show_footer=False, box=dashBox, highlight=True, border_style=tblBrdrStyle, style=tblTtlStyle)  
         self.tblMarket.add_column("", style=colDescStyle)
         self.tblMarket.add_column("", justify='right', style=colValStyle)
-        
+    
         self.tblMarket.add_row(
             Text(f"{'Price'}"),
             Text(f"${self.BTCPrice:,.0f}", style=ValueColor(round(self.BTCPrice), round(PreviousSelf.BTCPrice)))
         )
-                    
+                
         self.tblMarket.add_row(
             Text(f"{'Sats per Dollar'}"),
             Text(f"{self.satusd:,.0f}", style=ValueColor(round(self.satusd), round(PreviousSelf.satusd))),
@@ -367,7 +375,7 @@ class dashboard(object):
             Text(f"{'Market Capitalization'}"),
             Text(f"${self.MarketCap:.1f}B", style=ValueColor(round(self.MarketCap,1), round(PreviousSelf.MarketCap,1)))
         )
-    
+
         #Gold
         self.tblGold = Table(title=' Gold', title_justify='left', show_header=False, show_footer=False, width = tblwidth, box=dashBox, highlight=True, border_style=tblBrdrStyle, style=tblTtlStyle)  
         self.tblGold.add_column("", style=colDescStyle)
@@ -380,7 +388,7 @@ class dashboard(object):
             Text(f"{'Bitcoin vs. Gold Market Cap'}"),
             Text(f"{self.BTCvsGOLDMarketCap:.2f}%", style=ValueColor(round(self.BTCvsGOLDMarketCap,2), round(PreviousSelf.BTCvsGOLDMarketCap,2)))
         )
-    
+
         #Supply
         self.tblSupply = Table(title=' Supply', title_justify='left', show_header=False, show_footer=False, width = tblwidth, box=dashBox, highlight=True, border_style=tblBrdrStyle, style=tblTtlStyle)  
         self.tblSupply.add_column("", style=colDescStyle)
@@ -396,13 +404,13 @@ class dashboard(object):
         self.tblSupply.add_row(
             Text(f"{'Unspendable'}"),
             Text(f"{self.UNSPENDABLE:.2f}", style=ValueColor(round(self.UNSPENDABLE,2), round(PreviousSelf.UNSPENDABLE,2)))
-   
+
         )
         self.tblSupply.add_row(
             Text(f"{'Issuance Remaining'}"),
             Text(f"{self.IssuanceRemaining:,.2f}", style=ValueColor(round(self.IssuanceRemaining,2), round(PreviousSelf.IssuanceRemaining,2))),
         )
-    
+
         # Mining Economics
         self.tblMining = Table(title=' Mining Economics', title_justify='left', show_header=False, show_footer=False, width = tblwidth, box=dashBox, highlight=True, border_style=tblBrdrStyle, style=tblTtlStyle)  
         self.tblMining.add_column("", style=colDescStyle)
@@ -415,7 +423,7 @@ class dashboard(object):
             Text(f"{'Subsidy value'}"),
             Text(f"${self.blockSubsidyValue:,.0f}", style=ValueColor(round(self.blockSubsidyValue), round(PreviousSelf.blockSubsidyValue)))
         )
-    
+
         #Best Block Summary
         self.tblBestBlock = Table(title=' Best Block Summary', title_justify='left', show_header=False, show_footer=False, width = tblwidth, box=dashBox, highlight=True, border_style=tblBrdrStyle, style=tblTtlStyle) 
         self.tblBestBlock.add_column("", style=colDescStyle)
@@ -424,7 +432,7 @@ class dashboard(object):
             Text(f"{'Block Height'}"),
             Text(f"{self.MAX_HEIGHT:,.0f}", style=ValueColor(self.MAX_HEIGHT, PreviousSelf.MAX_HEIGHT)),
         )
-        
+    
         self.tblBestBlock.add_row(
             Text(f"{'Chain size'}"),
             Text(f"{self.chainSize:.1f} GB", style=ValueColor(round(self.chainSize,1), round(PreviousSelf.chainSize,1))),
@@ -445,7 +453,7 @@ class dashboard(object):
             Text(f"{'Time'}"),
             Text(f"{self.BestBlockAge} ago")
         )
-    
+
         #Network Summary
         self.tblNetwork = Table(title=' Network Summary', title_justify='left', show_header=False, show_footer=False, width = tblwidth, box=dashBox, highlight=True, border_style=tblBrdrStyle, style=tblTtlStyle) 
         self.tblNetwork.add_column("", style=colDescStyle)
@@ -458,7 +466,7 @@ class dashboard(object):
             Text(f"{'  Inbound'}"),
             Text(f"{self.ConnectionsIn}", style=ValueColor(self.ConnectionsIn, PreviousSelf.ConnectionsIn)),
         )
-    
+
         self.tblNetwork.add_row(
             Text(f"{'Verification Progress'}"),
             Text(f"{self.verification:,.04f}%", style=ValueColor(round(self.verification,4), round(PreviousSelf.verification,4))),
@@ -495,7 +503,7 @@ class dashboard(object):
             Text(f"{'  Count, 30 Days'}"),
             Text(f"{self.txCount:,.0f}", style=ValueColor(self.txCount, PreviousSelf.txCount))
         )
-    
+
         #Metrics / Events
         self.tblMetricEvents = Table(title=' Metrics / Events', title_justify='left', show_header=False, show_footer=False, width = tblwidth,  box=dashBox, 
                                 highlight=True, border_style=tblBrdrStyle, style=tblTtlStyle) 
@@ -541,7 +549,7 @@ class dashboard(object):
             Text(f"{'  Subsidy Epoch'}"),
             Text(f"{self.subsidyEpoch}", style=ValueColor(self.subsidyEpoch, PreviousSelf.subsidyEpoch))  
         )
-        
+    
         #MempoolInfo
         self.tblMempoolInfo = Table(title=' Mempool Info', title_justify='left', show_header=False, show_footer=False, width = tblwidth, box=dashBox, 
                                 highlight=True, border_style=tblBrdrStyle, style=tblTtlStyle) 
@@ -551,7 +559,7 @@ class dashboard(object):
             Text(f"{'Transactions'}"),
             Text(f"{self.mempool_txs:,.0f}", style=ValueColor(self.mempool_txs, PreviousSelf.mempool_txs)),
         )
-        
+    
         self.tblMempoolInfo.add_row(
             Text(f"{'Total Fee'}"),
             Text(f"{self.total_fee:.2f} BTC", style=ValueColor(round(self.total_fee,2), round(PreviousSelf.total_fee,2))),
@@ -569,9 +577,17 @@ class dashboard(object):
             Text(f"{'Size in bytes'}"),
             Text(f"{self.mempool_bytes/1000000:,.1f} MB", style=ValueColor(round((self.mempool_bytes/1000000),1), round((PreviousSelf.mempool_bytes/1000000),1)))
             )
+        self.tblMempoolInfo.add_row(
+            Text(f"{'Blocks to Clear'}"),
+            Text(f"{1+((self.mempool_bytes/1000000)//4):,.0f}", style=ValueColor(round((1+((self.mempool_bytes/1000000)//4)),0), round((1+((PreviousSelf.mempool_bytes/1000000)//4)),1)))
+            )
+        self.tblMempoolInfo.add_row(
+            Text(f"{'Percent RBF'}"),
+            Text(f"{self.pct_rbf:.1f}%", style=ValueColor(round(self.pct_rbf,1), round(PreviousSelf.pct_rbf,1)))
+            )
+    
 
         PreviousSelf = self
-
 
         return self.tblMarket, self.tblGold, self.tblSupply, self.tblMining, self.tblBestBlock, self.tblNetwork, self.tblMetricEvents, self.tblMempoolInfo 
 
@@ -579,6 +595,8 @@ class dashboard(object):
 __all__ = ('generateDataForTables'
            ,'dashboard'
 )
+
+
 
 
 
